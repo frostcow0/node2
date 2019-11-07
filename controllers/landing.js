24256b1ba00669c8bb94995e009270781c2c5f69
Jon Martin <gametime785@gmail.com>
2019-11-07 10:53:26 -0600

    Fixing things and making them work. Added the detail page.

--- controllers/landing.js
+++ controllers/landing.js
@@ -8,6 +8,25 @@ exports.submit_lead = function(req, res, next) {
   return models.Lead.create({
     email:req.body.lead_email
   }).then(lead=> {
-    res.redirect('/');
+    res.redirect('/leads');
   })
 };
+
+exports.show_leads = function(req,res,next) {
+    return models.Lead.findAll().then(leads=> {
+      res.render('landing', {title: 'Express', leads:leads});
+    })
+}
+
+exports.show_lead = function(req,res,next) {
+  return models.Lead.findOne({
+    where: {
+      id: req.params.lead_id
+    }
+  }).then(lead=> {
+    res.render('lead',{lead:lead});
+  });
+  return models.Lead.findAll().then(leads=> {
+    res.render('landing',{title:'Express',leads:leads});
+  })
+}
