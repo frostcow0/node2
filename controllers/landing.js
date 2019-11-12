80536637617cb45432098c4109dd9992effc3189
Jon Martin <gametime785@gmail.com>
2019-11-12 10:48:30 -0600

    Progress, but still an error.

--- controllers/landing.js
+++ controllers/landing.js
@@ -26,7 +26,29 @@ exports.show_lead = function(req,res,next) {
   }).then(lead=> {
     res.render('lead',{lead:lead});
   });
-  return models.Lead.findAll().then(leads=> {
-    res.render('landing',{title:'Express',leads:leads});
+}
+
+exports.show_edit_lead = function(req,res,next) {
+  return models.Lead.findOne({
+    where: {
+      id: req.params.lead_id
+    }
+  }).then(lead=> {
+    res.render('lead/edit_lead',{lead:lead});
+  });
+}
+
+exports.edit_lead = function(req,res,next) {
+  req.params.lead_id
+  req.body.lead_email
+
+  return models.Lead.update({
+    email:req.body.lead_email
+  }, {
+    where: {
+      id: req.params.lead_id
+    }
+  }).then(result=> {
+    res.redirect('/lead/'+req.params.lead_id);
   })
 }
